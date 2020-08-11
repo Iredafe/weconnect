47b2c286c5f149e5ee0963653799277d6e32566d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-08-11 11:33:16 -0700

    create user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,37 +1,68 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const User = require('../../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //@route    POST api/users
 //@desc     Register user
 //@Access   public
+
 router.post(
   '/',
   [
-    // username must be an email
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    // password must be at least 5 chars long
     check(
       'password',
       'Please include a valid password with at least 6 characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    //See if the user exists
+    const { name, email, password } = req.body;
+    try {
+      //See if the user exists
 
-    //Get the gravatar
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+      //Get the gravatar
 
-    //Encrypt password
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
-    //return Jsonwebtoken
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
-    res.send('User route');
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
